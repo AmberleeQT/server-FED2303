955515d0fbb9adea34d75f0b76c4cf2f2be29f2e
Amberlee Allmond <aallmondcar@gmail.com>
2023-05-18 12:08:06 -0400

    root level login authentication

--- index.js
+++ index.js
@@ -26,6 +26,23 @@ const db = require('./db')
 const User = require('./models/users');
 const Post = require('./models/posts');
 
+// Root-l;evel rout
+app.post('/login', async (req, res) => {
+  const username= req.body.username;
+  const user = await User.findOne({
+    where:{
+      username
+    }
+  })
+  if(user){
+    req.session.userInfo = {username: user.username}
+    req.session.save()
+    res.redirect('user-view')
+  } else {
+    res.sendStatus(400)
+  }
+})
+
 // View Engine
 const { engine } = require('express-handlebars');
 app.engine('handlebars', engine()) // registers app to use this particular view engine: 'engine', handler
